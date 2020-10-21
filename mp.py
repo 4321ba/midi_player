c24b9d6e5dab421cb30e2594b4cfd8a118e1d31c
1234ab <nevenincs007@gmail.com>
2020-10-21 12:12:25 +0200

    add shiftpitch and revamp input files

--- mp.py
+++ mp.py
@@ -8,20 +8,28 @@ import wiringpi
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="play midi files on RaspberryPi")
-    parser.add_argument("input_file", nargs="?", default = "", help="midi file to open, leave blank for random in current dir")
-    parser.add_argument("-l", "--loop", type=int, default = 1, metavar="count", help="play more midi files")
-    parser.add_argument("-s", "--speed", type=float, default = 1, metavar="multiplier", help="multiplies the playback speed")
+    parser.add_argument("input_files", nargs="*", default = ['.'], help="midi file(s) to play, or dir(s) to get midi(s) from, random play if more midis are available")
+    parser.add_argument("-c", "--count", type=int, default = 1, metavar="count", help="specify the maximum number of midi files to play, default is 1")
+    parser.add_argument("-s", "--speed", type=float, default = 1, metavar="multiplier", help="makes the music speed slower or faster")
+    parser.add_argument("-S", "--shiftpitch", type=int, default = 0, metavar="semitones", help="makes the music pitch lower or higher")
     parser.add_argument("-p", "--pedal", action='store_true', dest="use_pedal", help="it uses pedal information")
     parser.add_argument("-n", "--nonverbose", action='store_true', dest="is_silent", help="it doesn't print to stdout")
     parser.add_argument("-P", "--pins", default = "21,20,16,12,1,7,8,25,24,23", metavar="numbers", help="determines the number of piezo speakers too, default is \"21,20,16,12,1,7,8,25,24,23\"")
     return vars(parser.parse_args())
 
-def select_input_file(input_file):
-    if input_file == "":
-        midi_files = [i for i in os.listdir() if i[-4:] == ".mid"]
-        return random.choice(midi_files)
-    else:
-        return input_file
+def get_possible_midi_files(input_files, is_silent):
+    new_input_files = []
+    for i in input_files:
+        if i[-4:] == ".mid":
+            if os.path.isfile(i):
+                new_input_files.append(i)
+            elif not is_silent:
+                print("didn't find midi file: " + i)
+        elif os.path.isdir(i):
+            new_input_files += [os.path.join(i, j) for j in os.listdir(i) if j[-4:] == ".mid"]
+        elif not is_silent:
+            print("didn't find midi file nor dir named: " + i)
+    return new_input_files
 
 def import_midi_file(input_file, is_silent):
     if not is_silent:
@@ -76,16 +84,16 @@ def convert_time(data, speed):
             time_since_note = 0
     return new_data, length, note_count
 
-def calculate_hz(midi_note):
+def calculate_hz(midi_note, shiftpitch):
     if midi_note < 45:
-        return int(440*2**((midi_note-69)/12))
+        return int(440*2**((midi_note+shiftpitch-69)/12))
     else:
-        return int(440*2**((midi_note-69)/12)*(((midi_note-45)*0.005)**2+1))
+        return int(440*2**((midi_note+shiftpitch-69)/12)*(((midi_note-45)*0.005)**2+1))
 
-def restrict_number_of_notes(data, pins): #data: [[s az előzőtől, on-e, hgmagasság],[]]
-    new_data = []                         #[[s az előzőtől, pin, hgmagasság],[]]
-    sounding_notes = []                   #[[pin, hgmagasság],[]]
-    bad_offs = []                         #[kitolt hgmagasság, ...]
+def restrict_number_of_notes(data, pins, shiftpitch): #data: [[s az előzőtől, on-e, hgmagasság],[]]
+    new_data = []           #[[s az előzőtől, pin, hgmagasság],[]]
+    sounding_notes = []     #[[pin, hgmagasság],[]]
+    bad_offs = []           #[kitolt hgmagasság, ...]
     delay_until_good_off = 0
     bad_offs_count = 0
     for i in data:
@@ -94,14 +102,14 @@ def restrict_number_of_notes(data, pins): #data: [[s az előzőtől, on-e, hgmag
                 shifted_note = sounding_notes.pop(0)
                 bad_offs.append(shifted_note[1])
                 sounding_notes.append([shifted_note[0], i[2]])
-                new_data.append([i[0] + delay_until_good_off, shifted_note[0], calculate_hz(i[2])])
+                new_data.append([i[0] + delay_until_good_off, shifted_note[0], calculate_hz(i[2], shiftpitch)])
                 delay_until_good_off = 0
                 bad_offs_count += 1
             else:
                 for j in pins:
                     if not any(j == k[0] for k in sounding_notes):
                         sounding_notes.append([j, i[2]])
-                        new_data.append([i[0] + delay_until_good_off, j, calculate_hz(i[2])])
+                        new_data.append([i[0] + delay_until_good_off, j, calculate_hz(i[2], shiftpitch)])
                         delay_until_good_off = 0
                         break
         else:
@@ -131,17 +139,19 @@ def play(data):
 
 def main():
     args = parse_arguments()
+    input_files = get_possible_midi_files(args["input_files"], args["is_silent"])
+    random.shuffle(input_files)
+    input_files = input_files[0:args["count"]]
     pins = [int(i) for i in args["pins"].split(",")]
     wiringpi.wiringPiSetupGpio()
     [wiringpi.softToneCreate(i) for i in pins]
-    for i in range(args["loop"]):
+    for i in input_files:
         stats = {"sustained_notes": 0, "length": 0, "note_count": 0, "bad_offs_count": 0}
-        input_file = select_input_file(args["input_file"])
-        data = import_midi_file(input_file, args["is_silent"])
+        data = import_midi_file(i, args["is_silent"])
         data.sort(key = lambda x: int(x[1]))
         data, stats["sustained_notes"] = pedal_to_noteoff(data, args["use_pedal"])
         data, stats["length"], stats["note_count"] = convert_time(data, args["speed"])
-        data, stats["bad_offs_count"] = restrict_number_of_notes(data, pins)
+        data, stats["bad_offs_count"] = restrict_number_of_notes(data, pins, args["shiftpitch"])
         if not args["is_silent"]:
             show_stats(stats)
         play(data)
@@ -150,5 +160,5 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("stopped playing")
+        print("interrupted by keyboard")
         raise SystemExit
